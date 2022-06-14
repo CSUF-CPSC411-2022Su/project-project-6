c8aebac11d199936c95524a238c56e058db123df
Smit T <smittrambadia@csu.fullerton.edu>
2022-06-13 21:11:28 -0700

    Initial Commit

---
+++ GIFGo/GIFGo/GIFGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIFGoApp.swift
+//  GIFGo
+//
+//  Created by csuftitan on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GIFGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
