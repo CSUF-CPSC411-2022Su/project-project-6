94adab087dfb148a2291bd7fcc1b6c2c0aa8bba7
Rushik <rush.ramani@gmail.com>
2022-06-14 00:09:31 -0700

    Setting code and file structure committed

---
+++ GIFGo/GIF Maker/ViewController/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  GIFGo
+//
+//  Created by csuftitan on 6/14/22.
+
+import Foundation
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Settings"
+    }
+}
+
