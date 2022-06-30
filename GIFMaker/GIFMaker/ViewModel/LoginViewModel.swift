9d2d6f2a639f55bcd345a25d76d1a555b3330b66
smit2017 <106931893+smit2017@users.noreply.github.com>
2022-06-30 14:58:17 -0700

    Update LoginViewModel.swift

--- GIFMaker/GIFMaker/ViewModel/LoginViewModel.swift
+++ GIFMaker/GIFMaker/ViewModel/LoginViewModel.swift
@@ -42,6 +42,31 @@ class LoginViewModel: ObservableObject {
         }
     }
     
+    //Google Signin API
+    func signIn() {
+        // 1
+        if GIDSignIn.sharedInstance.hasPreviousSignIn() {
+            GIDSignIn.sharedInstance.restorePreviousSignIn { [unowned self] user, error in
+                authenticateUser(for: user, with: error)
+            }
+        } else {
+            // 2
+            guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+            // 3
+            let configuration = GIDConfiguration(clientID: clientID)
+        
+            // 4
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+            guard let rootViewController = windowScene.windows.first?.rootViewController else { return }
+        
+            // 5
+            GIDSignIn.sharedInstance.signIn(with: configuration, presenting: rootViewController) { [unowned self] user, error in
+                authenticateUser(for: user, with: error)
+            }
+        }
+    }
+    
     
     
     
