2f0e15a77ebc7bb6d30bb7ae26166a3409018132
KevinJasani <107000227+KevinJasani@users.noreply.github.com>
2022-06-30 01:12:03 -0700

    Update LoginViewModel.swift

--- GIFMaker/GIFMaker/ViewModel/LoginViewModel.swift
+++ GIFMaker/GIFMaker/ViewModel/LoginViewModel.swift
@@ -26,6 +26,24 @@ class LoginViewModel: ObservableObject {
     @Published var showError: Bool = false
     @Published var errorMsg: String = ""
     
+    // Note: Firebase Login
+    func loginUser(useFaceID: Bool, email: String = "", password: String = "") async throws {
+        let _ = try await Auth.auth().signIn(withEmail: email != "" ? email : self.email, password: password != "" ? password : self.password)
+        
+        DispatchQueue.main.async {
+            // Storing Once
+            if useFaceID, self.faceIDEmail == "" {
+                self.useFaceID = useFaceID
+                // Note: Storing for Future FaceID Login
+                self.faceIDEmail = self.email
+                self.faceIDPassword = self.password
+            }
+            
+            self.logStatus = true
+        }
+    }
+    
+    
     // Note: FaceID Usage
     func getBioMetricStatus() -> Bool {
         let scanner = LAContext()
@@ -42,9 +60,64 @@ class LoginViewModel: ObservableObject {
         }
     }
     
+    private func authenticateUser(for user: GIDGoogleUser?, with error: Error?) {
+        // 1
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+      
+        // 2
+        guard let authentication = user?.authentication, let idToken = authentication.idToken else { return }
+      
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+      
+        // 3
+        Auth.auth().signIn(with: credential) { [unowned self] _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                // self.state = .signedIn
+                self.logStatus = true
+            }
+        }
+    }
     
+    func signIn() {
+        // 1
+        if GIDSignIn.sharedInstance.hasPreviousSignIn() {
+            GIDSignIn.sharedInstance.restorePreviousSignIn { [unowned self] user, error in
+                authenticateUser(for: user, with: error)
+            }
+        } else {
+            // 2
+            guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+            // 3
+            let configuration = GIDConfiguration(clientID: clientID)
+        
+            // 4
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+            guard let rootViewController = windowScene.windows.first?.rootViewController else { return }
+        
+            // 5
+            GIDSignIn.sharedInstance.signIn(with: configuration, presenting: rootViewController) { [unowned self] user, error in
+                authenticateUser(for: user, with: error)
+            }
+        }
+    }
     
-    
-    
-    
+    func signOut() {
+        // 1
+        GIDSignIn.sharedInstance.signOut()
+      
+        do {
+            // 2
+            try Auth.auth().signOut()
+            logStatus = false
+            // state = .signedOut
+        } catch {
+            print(error.localizedDescription)
+        }
+    }  
 }
