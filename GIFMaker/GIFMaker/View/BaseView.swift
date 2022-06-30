4beff6731a1732f4f8b6948620347878574764e1
KevinJasani <107000227+KevinJasani@users.noreply.github.com>
2022-06-30 01:13:41 -0700

    Update BaseView.swift

--- GIFMaker/GIFMaker/View/BaseView.swift
+++ GIFMaker/GIFMaker/View/BaseView.swift
@@ -31,3 +31,40 @@ struct DetailView: View {
         }
     }
 }
+
+struct gif_details_view: View {
+    @State public var details_gif = false
+    @AppStorage("name") var dynamic_download_gif: String?
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(dynamic_download_gif ?? "Add Name Here")
+
+            Button("Click Here".uppercased()) {
+                let download_gif_appstorage = "Login Feature is Created By Kevin"
+                dynamic_download_gif = download_gif_appstorage
+            }
+            .padding(.horizontal, 30)
+            .foregroundColor(.white)
+            .background(Color("CherryRed"))
+        }
+        
+        List(0..<4) { item in
+            GifImage(gifs[item])
+                .frame(width: 200, height: 200)
+            
+            NavigationLink(destination: gifs_details(), label: {
+                Text("Show Details")
+            })
+        }
+    }
+}
+
+struct gifs_details: View {
+    var body: some View {
+        Text("Name: MarioGIF").frame(alignment: .leading).padding(10)
+        Text("Type of file: GIF File (.gif)").frame(alignment: .leading).padding(10)
+        Text("Opens with: GIFs").frame(alignment: .leading).padding(10)
+        Text("File Size: 140KB").frame(alignment: .leading).padding(10)
+        Text("Location: Downloads").frame(alignment: .leading).padding(10)
+    }
+}
