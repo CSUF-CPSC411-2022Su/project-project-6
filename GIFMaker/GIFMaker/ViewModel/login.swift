1fd6e006b560a190bc83c1c0ceb8ca8e6de90277
Smit T <smittrambadia@csu.fullerton.edu>
2022-06-29 21:58:35 -0700

    Folder Structure

---
+++ GIFMaker/GIFMaker/ViewModel/login.swift
@@ -0,0 +1,20 @@
+//
+//  login.swift
+//  GIFMaker
+//
+//  Created by csuftitan on 6/29/22.
+//
+
+import SwiftUI
+
+struct login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct login_Previews: PreviewProvider {
+    static var previews: some View {
+        login()
+    }
+}
