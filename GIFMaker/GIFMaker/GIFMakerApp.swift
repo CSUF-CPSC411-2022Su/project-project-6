764f4a7f57410f2c0bc5d2643af022e4aea5aa29
Smit T <smittrambadia@csu.fullerton.edu>
2022-06-29 21:40:01 -0700

    Directory Structure

---
+++ GIFMaker/GIFMaker/GIFMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIFMakerApp.swift
+//  GIFMaker
+//
+//  Created by csuftitan on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct GIFMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
